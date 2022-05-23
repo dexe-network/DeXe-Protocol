096f88417b0532fdaf5204055c8ab9cb2d02a5f2
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-05-23 15:26:16 +0300

    added test for snapshot

--- test/GovUserKeeper.test.js
+++ test/GovUserKeeper.test.js
@@ -419,11 +419,233 @@ describe("GovUserKeeper", () => {
     });
 
     describe("check snapshot", async () => {
+      let startTime;
+      let enumUserKeeper;
+      let powerUserKeeper;
+      let poolWithNftBase;
+      let poolWithNftEnum;
+      let poolWithNftPower;
+
+      let nftEnum;
+      let nftPower;
+
+      const INTERNAL_SETTINGS = {
+        earlyCompletion: true,
+        duration: 500,
+        durationValidators: 600,
+        quorum: PRECISION.times("51").toFixed(),
+        quorumValidators: PRECISION.times("61").toFixed(),
+        minTokenBalance: wei("10"),
+        minNftBalance: 2,
+      };
+
+      const DEFAULT_SETTINGS = {
+        earlyCompletion: false,
+        duration: 700,
+        durationValidators: 800,
+        quorum: PRECISION.times("71").toFixed(),
+        quorumValidators: PRECISION.times("100").toFixed(),
+        minTokenBalance: wei("20"),
+        minNftBalance: 3,
+      };
+
       beforeEach("setup", async () => {
+        startTime = await getCurrentBlockTime();
+
+        nftEnum = await ERC721EnumMock.new("Mock", "Mock");
+        nftPower = await ERC721Power.new("Mock", "Mock", startTime + 100);
+        await nftPower.setMaxPower("10000");
+        await nftPower.setRequiredCollateral("500");
+        await nftPower.setReductionPercent(toPercent("0.01"));
+
+        enumUserKeeper = await GovUserKeeper.new();
+        powerUserKeeper = await GovUserKeeper.new();
+
+        await enumUserKeeper.__GovUserKeeper_init(token.address, nftEnum.address, wei("33000"), 33);
+        await powerUserKeeper.__GovUserKeeper_init(token.address, nftPower.address, wei("33000"), 33);
+
+        for (let i = 1; i < 10; i++) {
+          if (i === 8) continue;
+          await nftEnum.safeMint(OWNER, i);
+          await nftEnum.approve(enumUserKeeper.address, i);
+          await nftPower.safeMint(OWNER, i);
+          await nftPower.approve(powerUserKeeper.address, i);
+        }
+
+        await nftPower.setCollateralToken(token.address);
+        await token.approve(nftPower.address, wei("500"));
+        await nftPower.addCollateral(wei("500"), "9");
+
+        let settingsWithNftBase = await GovSettings.new();
+        await settingsWithNftBase.__GovSettings_init(INTERNAL_SETTINGS, DEFAULT_SETTINGS);
+        let settingWithNftEnum = await GovSettings.new();
+        await settingWithNftEnum.__GovSettings_init(INTERNAL_SETTINGS, DEFAULT_SETTINGS);
+        let settingWithNftPower = await GovSettings.new();
+        await settingWithNftPower.__GovSettings_init(INTERNAL_SETTINGS, DEFAULT_SETTINGS);
+
+        // let executorWithNftBase = await ExecutorMock.new();
+        // let executorWithNftEnum = await ExecutorMock.new();
+        // let executorWithNftPower = await ExecutorMock.new();
+
+        let validatorsNftBase = await GovValidators.new();
+        await validatorsNftBase.__GovValidators_init(
+          "Validator Token",
+          "VT",
+          600,
+          PRECISION.times("51"),
+          [OWNER],
+          [wei("100")]
+        );
+
+        let validatorsNftEnum = await GovValidators.new();
+        await validatorsNftEnum.__GovValidators_init(
+          "Validator Token",
+          "VT",
+          600,
+          PRECISION.times("51"),
+          [OWNER],
+          [wei("100")]
+        );
+
+        let validatorsNftPower = await GovValidators.new();
+        await validatorsNftPower.__GovValidators_init(
+          "Validator Token",
+          "VT",
+          600,
+          PRECISION.times("51"),
+          [OWNER],
+          [wei("100")]
+        );
+
+        poolWithNftBase = await GovPool.new();
+        poolWithNftBase.__GovPool_init(
+          settingsWithNftBase.address,
+          userKeeper.address,
+          validatorsNftBase.address,
+          10,
+          toPercent("10")
+        );
+        poolWithNftEnum = await GovPool.new();
+        poolWithNftEnum.__GovPool_init(
+          settingWithNftEnum.address,
+          enumUserKeeper.address,
+          "0x0000000000000000000000000000000000000000",
+          10,
+          toPercent("10")
+        );
+        poolWithNftPower = await GovPool.new();
+        poolWithNftPower.__GovPool_init(
+          settingWithNftPower.address,
+          powerUserKeeper.address,
+          "0x0000000000000000000000000000000000000000",
+          10,
+          toPercent("10")
+        );
+
+        await settingsWithNftBase.transferOwnership(poolWithNftBase.address);
+        await settingWithNftEnum.transferOwnership(poolWithNftEnum.address);
+        await settingWithNftPower.transferOwnership(poolWithNftPower.address);
+
+        await userKeeper.transferOwnership(poolWithNftBase.address);
+        await enumUserKeeper.transferOwnership(poolWithNftEnum.address);
+        await powerUserKeeper.transferOwnership(poolWithNftPower.address);
+
+        await validatorsNftBase.transferOwnership(poolWithNftBase.address);
+        await validatorsNftEnum.transferOwnership(poolWithNftEnum.address);
+        await validatorsNftPower.transferOwnership(poolWithNftPower.address);
+
+        await token.mint(OWNER, wei("900"));
+        await token.mint(SECOND, wei("900"));
+        await token.approve(poolWithNftBase.address, wei("900"), { from: SECOND });
+        await token.approve(userKeeper.address, wei("900"));
+        await userKeeper.depositTokens(THIRD, wei("900"));
+
         await userKeeper.depositNfts(OWNER, [1]);
+        await enumUserKeeper.depositNfts(OWNER, [1]);
+        await powerUserKeeper.depositNfts(OWNER, [1]);
       });
 
-      it("should correctly calculate NFT power after snapshot", async () => {});
+      it("should correctly calculate NFT power after snapshot", async () => {
+        await setTime(startTime + 999 + 100);
+        await poolWithNftPower.createProposal([SECOND], ["0x00"]);
+
+        await setTime(startTime + 1999 + 100);
+        await poolWithNftPower.createProposal([SECOND], ["0x00"]);
+
+        assert.equal((await powerUserKeeper.nftSnapshot(1)).totalNftsPower.toString(), "73000");
+        assert.equal(
+          (await powerUserKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 1)).toFixed(),
+          wei("4068.493150684931506849")
+        );
+        assert.equal((await powerUserKeeper.getNftsPowerInTokens({ values: [8], length: 1 }, 1)).toFixed(), "0");
+        assert.equal(
+          (await powerUserKeeper.getNftsPowerInTokens({ values: [9], length: 1 }, 1)).toFixed(),
+          wei("5020.547945205479452054")
+        );
+        assert.equal(
+          (await powerUserKeeper.getNftsPowerInTokens({ values: [1, 8, 9], length: 3 }, 1)).toFixed(),
+          wei("9089.041095890410958903")
+        );
+
+        assert.equal((await powerUserKeeper.nftSnapshot(2)).totalNftsPower.toFixed(), "66000");
+        assert.equal(
+          (await powerUserKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 2)).toFixed(),
+          wei("4000")
+        );
+        assert.equal((await powerUserKeeper.getNftsPowerInTokens({ values: [8], length: 1 }, 2)).toFixed(), "0");
+        assert.equal(
+          (await powerUserKeeper.getNftsPowerInTokens({ values: [9], length: 1 }, 2)).toFixed(),
+          wei("5500")
+        );
+      });
+
+      it("should correctly calculate NFT weight when support only totalSupply()", async () => {
+        await setTime(startTime + 999);
+        await poolWithNftEnum.createProposal([OWNER], ["0x00"]);
+        await nftEnum.safeMint(OWNER, 10);
+
+        await setTime(startTime + 1999);
+        await poolWithNftEnum.createProposal([OWNER], ["0x00"]);
+
+        assert.equal((await enumUserKeeper.nftSnapshot(1)).totalSupply.toFixed(), "8");
+        assert.equal((await enumUserKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 1)).toFixed(), wei("4125"));
+        assert.equal((await enumUserKeeper.nftSnapshot(2)).totalSupply.toFixed(), "9");
+        assert.equal(
+          (await enumUserKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 2)).toFixed(),
+          wei("3666.666666666666666666")
+        );
+        assert.equal(
+          (await enumUserKeeper.getNftsPowerInTokens({ values: [1, 10], length: 2 }, 2)).toFixed(),
+          wei("7333.333333333333333333")
+        );
+      });
+
+      it("should correctly calculate NFT weight when totalSupply() and power is not supported", async () => {
+        await setTime(startTime + 999);
+        await poolWithNftBase.createProposal([OWNER], ["0x00"]);
+
+        await setTime(startTime + 1999);
+        await poolWithNftBase.createProposal([OWNER], ["0x00"]);
+
+        assert.equal((await userKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 1)).toFixed(), wei("1000"));
+        assert.equal((await userKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 2)).toFixed(), wei("1000"));
+        assert.equal((await userKeeper.getNftsPowerInTokens({ values: [1, 2], length: 2 }, 2)).toFixed(), wei("2000"));
+      });
+
+      it("should correctly calculate NFT weight if NFT contract is not added", async () => {
+        const newKeeper = await GovUserKeeper.new();
+        await newKeeper.__GovUserKeeper_init(
+          token.address,
+          "0x0000000000000000000000000000000000000000",
+          wei("33000"),
+          33
+        );
+
+        expect((await newKeeper.getNftsPowerInTokens({ values: [0], length: 1 }, 0)).toFixed(), "0");
+        expect((await newKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 0)).toFixed(), "0");
+        expect((await newKeeper.getNftsPowerInTokens({ values: [1], length: 1 }, 1)).toFixed(), "0");
+        expect((await newKeeper.getNftsPowerInTokens({ values: [0], length: 1 }, 1)).toFixed(), "0");
+      });
     });
   });
 });
