728014054a520c7c20fd37a58999d8eb0a96b4ab
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-06-01 13:27:06 +0300

    test for distribution proposal

--- test/DistributionProposal.test.js
+++ test/DistributionProposal.test.js
@@ -1,6 +1,8 @@
 const { assert } = require("chai");
 const { toBN, accounts, wei } = require("../scripts/helpers/utils");
 const truffleAssert = require("truffle-assertions");
+const { getCurrentBlockTime, setTime } = require("./helpers/hardhatTimeTraveller");
+const truffleAssertions = require("truffle-assertions");
 
 const GovPool = artifacts.require("GovPool");
 const DistributionProposal = artifacts.require("DistributionProposal");
@@ -18,6 +20,10 @@ ERC20Mock.numberFormat = "BigNumber";
 
 const PRECISION = toBN(10).pow(25);
 const ZERO = "0x0000000000000000000000000000000000000000";
+
+const getBytesExecute = () => {
+  return web3.eth.abi.encodeFunctionSignature("execute()");
+};
 
 const INTERNAL_SETTINGS = {
   earlyCompletion: true,
@@ -85,6 +91,124 @@ describe("DistributionProposal", () => {
       assert.equal(await proposal.govAddress(), govPool.address);
       assert.equal(await proposal.rewardAddress(), token.address);
       assert.equal((await proposal.rewardAmount()).toFixed(), wei("100000"));
+    });
+
+    it("should revert if `_govAddress` is zero", async () => {
+      await truffleAssert.reverts(
+        DistributionProposal.new("0x0000000000000000000000000000000000000000", token.address, wei("100000")),
+        "DP: `_govAddress` is zero"
+      );
+    });
+
+    it("should revert if `_rewardAddress` is zero", async () => {
+      await truffleAssert.reverts(
+        DistributionProposal.new(govPool.address, "0x0000000000000000000000000000000000000000", wei("100000")),
+        "DP: `_rewardAddress` is zero"
+      );
+    });
+
+    it("should revert if `_rewardAmount` is zero", async () => {
+      await truffleAssert.reverts(
+        DistributionProposal.new(govPool.address, token.address, wei("0")),
+        "DP: `_rewardAmount` is zero"
+      );
+    });
+  });
+
+  describe("setProposalId()", async () => {
+    it("should correctly set initial params", async () => {
+      await proposal.setProposalId(111);
+
+      assert.equal(await proposal.proposalId(), 111);
+    });
+
+    it("should revert if `_proposalId` is zero", async () => {
+      await truffleAssert.reverts(proposal.setProposalId(0), "DP: `_proposalId` is zero");
+    });
+
+    it("should revert if already set up", async () => {
+      await proposal.setProposalId(111);
+      await truffleAssert.reverts(proposal.setProposalId(0), "DP: already set up");
+    });
+  });
+
+  describe("execute()", async () => {
+    let startTime;
+    it("should correctly execute", async () => {
+      startTime = await getCurrentBlockTime();
+      await proposal.setProposalId(1);
+
+      await userKeeper.depositNfts(OWNER, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+      await setTime(startTime + 999);
+      await govPool.createProposal([proposal.address], [getBytesExecute()]);
+
+      await govPool.voteNfts(1, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+      await setTime(startTime + 2000);
+      await govPool.execute(1);
+
+      assert.equal(await proposal.distributionStarted(), true);
+    });
+
+    it("should revert if not a `Gov` contract", async () => {
+      await truffleAssert.reverts(proposal.execute(), "DP: not a `Gov` contract");
+    });
+  });
+
+  describe("claim()", async () => {
+    let startTime;
+    beforeEach(async () => {
+      startTime = await getCurrentBlockTime();
+
+      await token.mint(proposal.address, wei("100000"));
+
+      await userKeeper.depositNfts(SECOND, [1, 2, 3, 4, 5]);
+      await userKeeper.depositNfts(THIRD, [6, 7, 8, 9]);
+
+      await setTime(startTime + 999);
+      await govPool.createProposal([proposal.address], [getBytesExecute()], { from: SECOND });
+      await proposal.setProposalId(1);
+    });
+
+    it("should correctly claim", async () => {
+      await govPool.voteNfts(1, [1, 2, 3, 4, 5], { from: SECOND });
+      await govPool.voteNfts(1, [6, 7, 8, 9], { from: THIRD });
+
+      await setTime(startTime + 1700);
+      await govPool.execute(1);
+
+      await proposal.claim(SECOND);
+      await proposal.claim(THIRD);
+
+      assert.equal((await token.balanceOf(SECOND)).toFixed(), "55555555555555555555556");
+      assert.equal((await token.balanceOf(THIRD)).toFixed(), "44444444444444444444443");
+    });
+
+    it("should revert if already claimed", async () => {
+      await govPool.voteNfts(1, [1, 2, 3, 4, 5], { from: SECOND });
+      await govPool.voteNfts(1, [6, 7, 8, 9], { from: THIRD });
+
+      await setTime(startTime + 1700);
+      await govPool.execute(1);
+
+      await proposal.claim(SECOND);
+
+      await truffleAssert.reverts(proposal.claim(SECOND), "DP: already claimed");
+    });
+
+    it("should revert if nothing to claim", async () => {
+      await govPool.voteNfts(1, [1, 2, 3, 4, 5], { from: SECOND });
+      await govPool.voteNfts(1, [6, 7, 8, 9], { from: THIRD });
+
+      await setTime(startTime + 1700);
+      await govPool.execute(1);
+
+      await truffleAssertions.reverts(proposal.claim(OWNER), "DP: nothing to claim");
+    });
+
+    it("should revert if distribution isn't start yet", async () => {
+      await truffleAssert.reverts(proposal.claim(SECOND), "DP: distribution hasn't started yet");
     });
   });
 });
