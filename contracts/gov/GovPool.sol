4c485040aa290337262a4b3220730a53ebd8d4da
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-09-13 18:37:21 +0300

    fix

--- contracts/gov/GovPool.sol
+++ contracts/gov/GovPool.sol
@@ -67,8 +67,8 @@ contract GovPool is
     mapping(uint256 => mapping(address => uint256)) public pendingRewards; // proposalId => user => tokens amount
 
     event ProposalCreated(uint256 proposalId, address sender, uint256 quorum);
-    event Delegated(address to, uint256 amount);
-    event Undelegate(address to, uint256 amount);
+    event Delegated(address from, address to, uint256 amount);
+    event Undelegated(address from, address to, uint256 amount);
     event Voted(uint256 proposalId, address sender, uint256 personalVote, uint256 delegatedVote);
     event DPCreated(uint256 proposalId, address sender, address token, uint256 amount);
     // event ProposalAccepted(uint256 proposalId);
