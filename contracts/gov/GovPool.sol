ce0b2500caafa7133959e99b1ed4480655d80556
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-09-20 17:16:58 +0300

    moved ids array to delegete events

--- contracts/gov/GovPool.sol
+++ contracts/gov/GovPool.sol
@@ -67,10 +67,8 @@ contract GovPool is
     mapping(uint256 => mapping(address => uint256)) public pendingRewards; // proposalId => user => tokens amount
 
     event ProposalCreated(uint256 proposalId, address sender, uint256 quorum);
-    event Delegated(address from, address to, uint256 amount);
-    event Undelegated(address from, address to, uint256 amount);
-    event DelegatedNFT(address from, address to, uint256[] tokens);
-    event UndelegatedNFT(address from, address to, uint256[] tokens);
+    event Delegated(address from, address to, uint256 amount, uint256[] nfts);
+    event Undelegated(address from, address to, uint256 amount, uint256[] nfts);
     event Voted(uint256 proposalId, address sender, uint256 personalVote, uint256 delegatedVote);
     event DPCreated(uint256 proposalId, address sender, address token, uint256 amount);
     event ProposalExecuted(uint256 proposalId, address sender);
