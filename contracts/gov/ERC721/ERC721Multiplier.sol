eff3e73a3553f25c7eb44213918f99919ed2615d
dovgopoly <idovgopoly@gmail.com>
2022-11-08 16:07:06 +0200

    import whole globals file

--- contracts/gov/ERC721/ERC721Multiplier.sol
+++ contracts/gov/ERC721/ERC721Multiplier.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "../../interfaces/gov/ERC721/IERC721Multiplier.sol";
-import {PRECISION} from "../../core/Globals.sol";
+import "../../core/Globals.sol";
 
 contract ERC721Multiplier is IERC721Multiplier, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
