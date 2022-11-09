c7344ae62bd38a518c9d66d4d59efbc0a3c7702d
dovgopoly <idovgopoly@gmail.com>
2022-11-09 15:03:16 +0200

    coverage 100%

--- test/ERC721Multiplier.test.js
+++ test/ERC721Multiplier.test.js
@@ -1,7 +1,7 @@
 const { assert } = require("chai");
-const { toBN, accounts, wei, precision } = require("../scripts/utils/utils");
+const { accounts } = require("../scripts/utils/utils");
 const { setTime, getCurrentBlockTime } = require("./helpers/block-helper");
-const { PRECISION, ZERO_ADDR } = require("../scripts/utils/constants");
+const { PRECISION } = require("../scripts/utils/constants");
 const truffleAssert = require("truffle-assertions");
 
 const ERC721Multiplier = artifacts.require("ERC721Multiplier");
@@ -65,7 +65,14 @@ describe.only("ERC721Multiplier", () => {
     assert.equal(await nft.baseURI(), "");
   });
 
-  describe("mint()", async () => {
+  describe("interfaceId()", () => {
+    it("should support ERC721Enumerable and ERC721Multiplier interfaces", async () => {
+      assert.isTrue(await nft.supportsInterface("0x2aabfe18"));
+      assert.isTrue(await nft.supportsInterface("0x780e9d63"));
+    });
+  });
+
+  describe("mint()", () => {
     it("shouldn't mint if not the owner", async () => {
       await truffleAssert.reverts(
         nft.mint(OWNER, TOKENS[0].multiplier, TOKENS[0].duration, { from: SECOND }),
@@ -74,21 +81,29 @@ describe.only("ERC721Multiplier", () => {
     });
 
     it("should be zero totalSupply before minting", async () => {
-      assert.equal(await nft.totalSupply(), 0);
+      assert.equal(await nft.totalSupply(), "0");
     });
 
     it("should mint properly", async () => {
       for (const token of TOKENS) {
-        await nft.mint(token.owner, token.multiplier, token.duration);
+        const tx = await nft.mint(token.owner, token.multiplier, token.duration);
+        truffleAssert.eventEmitted(tx, "Minted", (e) => {
+          return (
+            e.to === token.owner &&
+            e.tokenId.toFixed() === token.id &&
+            e.multiplier.toFixed() === token.multiplier &&
+            e.duration.toFixed() === token.duration
+          );
+        });
       }
 
-      assert.equal(await nft.totalSupply(), 4);
-      assert.equal(await nft.balanceOf(SECOND), 2);
-      assert.equal(await nft.balanceOf(THIRD), 2);
-      assert.equal(await nft.tokenOfOwnerByIndex(SECOND, 0), 1);
-      assert.equal(await nft.tokenOfOwnerByIndex(SECOND, 1), 3);
-      assert.equal(await nft.tokenOfOwnerByIndex(THIRD, 0), 2);
-      assert.equal(await nft.tokenOfOwnerByIndex(THIRD, 1), 4);
+      assert.equal(await nft.totalSupply(), "4");
+      assert.equal(await nft.balanceOf(SECOND), "2");
+      assert.equal(await nft.balanceOf(THIRD), "2");
+      assert.equal(await nft.tokenOfOwnerByIndex(SECOND, "0"), "1");
+      assert.equal(await nft.tokenOfOwnerByIndex(SECOND, "1"), "3");
+      assert.equal(await nft.tokenOfOwnerByIndex(THIRD, "0"), "2");
+      assert.equal(await nft.tokenOfOwnerByIndex(THIRD, "1"), "4");
     });
   });
 
@@ -153,10 +168,44 @@ describe.only("ERC721Multiplier", () => {
         await nft.lock(first.id, { from: first.owner });
 
         await setTime(startTime + parseInt(first.duration) + 1);
+
         await nft.lock(second.id, { from: second.owner });
       });
     });
 
-    describe("isLocked()", () => {});
+    describe("isLocked()", () => {
+      it("should return `not locked` if nft has never been locked", async () => {
+        assert.isFalse(await nft.isLocked(TOKENS[0].id));
+      });
+
+      it("should return `locked` if nft is locked", async () => {
+        await nft.lock(TOKENS[0].id, { from: TOKENS[0].owner });
+        assert.isTrue(await nft.isLocked(TOKENS[0].id));
+      });
+
+      it("should return `not locked` if nft expired", async () => {
+        const startTime = await getCurrentBlockTime();
+        await nft.lock(TOKENS[0].id, { from: TOKENS[0].owner });
+        await setTime(startTime + parseInt(TOKENS[0].duration) + 2);
+        assert.isFalse(await nft.isLocked(TOKENS[0].id));
+      });
+    });
+
+    describe("getExtraRewards()", () => {
+      it("should return zero if no nft locked", async () => {
+        assert.equal(await nft.getExtraRewards(SECOND, "1000"), "0");
+      });
+
+      it("should return extra rewards properly", async () => {
+        await nft.lock(TOKENS[2].id, { from: TOKENS[2].owner });
+        assert.equal(await nft.getExtraRewards(SECOND, "1000"), "1500");
+      });
+
+      it("should return zero if nft is expired", async () => {
+        await nft.lock(TOKENS[2].id, { from: TOKENS[2].owner });
+        await setTime((await getCurrentBlockTime()) + parseInt(TOKENS[2].duration) + 1);
+        assert.equal(await nft.getExtraRewards(SECOND, "1000"), "0");
+      });
+    });
   });
 });
