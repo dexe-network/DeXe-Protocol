34f83051e83f312cb955ab1838f35f2dbd8fd65e
dovgopoly <idovgopoly@gmail.com>
2022-12-01 17:15:05 +0200

    sendFunds

--- contracts/libs/gov-pool/GovPoolStaking.sol
+++ contracts/libs/gov-pool/GovPoolStaking.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-import "@dlsl/dev-modules/libs/decimals/DecimalsConverter.sol";
 
 import "../../interfaces/gov/IGovPool.sol";
 import "../../interfaces/gov/user-keeper/IGovUserKeeper.sol";
 
+import "../utils/TokenBalance.sol";
 import "../math/MathHelper.sol";
 
 library GovPoolStaking {
+    using TokenBalance for address;
     using MathHelper for uint256;
-    using DecimalsConverter for uint256;
     using EnumerableSet for EnumerableSet.AddressSet;
-    using SafeERC20 for IERC20;
 
     function updateRewards(
         IGovPool.MicropoolInfo storage micropool,
@@ -87,10 +83,7 @@ library GovPoolStaking {
 
             delegatorInfo.pendingRewards = 0;
 
-            IERC20(rewardToken).safeTransfer(
-                msg.sender,
-                rewards.from18(ERC20(rewardToken).decimals())
-            );
+            rewardToken.sendFunds(msg.sender, rewards);
         }
     }
 }
