5ef790157253480f80754971a9f67b7a56849f4e
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-12-20 17:42:48 +0200

    doc for DP

--- contracts/interfaces/gov/proposals/IDistributionProposal.sol
+++ contracts/interfaces/gov/proposals/IDistributionProposal.sol
@@ -6,6 +6,10 @@ pragma solidity ^0.8.4;
  * all the voters who participated in the certain proposal
  */
 interface IDistributionProposal {
+    /// @notice The struct holds information about distribution proposal
+    /// @param rewardAddress the address of reward token
+    /// @param rewardAmount the total amount of reward
+    /// @param claimed mapping, that holds boolean flag, if true then user claimed reward
     struct DistributionProposalStruct {
         address rewardAddress;
         uint256 rewardAmount;
@@ -13,13 +17,18 @@ interface IDistributionProposal {
     }
 
     /// @notice Executed by `Gov` contract, open 'claim'
+    /// @param proposalId the id of distribution proposal, it is internal id of this contract
+    /// @param token the reward address token
+    /// @param amount the total amount of reward
     function execute(uint256 proposalId, address token, uint256 amount) external payable;
 
     /// @notice Distribute rewards
     /// @param voter Voter address
+    /// @param proposalIds the array of proposal ids
     function claim(address voter, uint256[] calldata proposalIds) external;
 
     /// @notice Return potential reward. If user hasn't voted, or `getTotalVotesWeight` is zero, return zero
+    /// @param proposalId the id of proposal
     /// @param voter Voter address
     function getPotentialReward(uint256 proposalId, address voter) external view returns (uint256);
 }
