741d95e8eb425a5dde7f99f6dbb3e15a8d5efb17
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-12-26 17:12:44 +0200

    changed migrations

--- deploy/3_CoreTokens.migration.js
+++ deploy/3_CoreTokens.migration.js
@@ -2,14 +2,13 @@ const { logTransaction } = require("@dlsl/hardhat-migrate");
 
 const Proxy = artifacts.require("TransparentUpgradeableProxy");
 const ContractsRegistry = artifacts.require("ContractsRegistry");
-
-// BSC TESTNET
-const usdAddress = "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7"; // BUSD
-const dexeAddress = "0xa651EdBbF77e1A2678DEfaE08A33c5004b491457";
+const ERC20Mock = artifacts.require("ERC20Mock");
 
 module.exports = async (deployer) => {
   const contractsRegistry = await ContractsRegistry.at((await Proxy.deployed()).address);
+  const USD = await deployer.deploy(ERC20Mock, "USD", "USD", 18);
+  const DEXE = await deployer.deploy(ERC20Mock, "DEXE", "DEXE", 18);
 
-  logTransaction(await contractsRegistry.addContract(await contractsRegistry.USD_NAME(), usdAddress), "Add USD");
-  logTransaction(await contractsRegistry.addContract(await contractsRegistry.DEXE_NAME(), dexeAddress), "Add DEXE");
+  logTransaction(await contractsRegistry.addContract(await contractsRegistry.USD_NAME(), USD.address), "Add USD");
+  logTransaction(await contractsRegistry.addContract(await contractsRegistry.DEXE_NAME(), DEXE.address), "Add DEXE");
 };
--- deploy/5_UniswapV2.migration.js
+++ deploy/5_UniswapV2.migration.js
@@ -1,22 +1,87 @@
-const { logTransaction } = require("@dlsl/hardhat-migrate");
+const { logTransaction, logContracts } = require("@dlsl/hardhat-migrate");
+const { toBN, wei } = require("../scripts/utils/utils");
 
 const Proxy = artifacts.require("TransparentUpgradeableProxy");
 const ContractsRegistry = artifacts.require("ContractsRegistry");
+const UniswapV2RouterMock = artifacts.require("UniswapV2RouterMock");
+const ERC20Mock = artifacts.require("ERC20Mock");
 
-// BSC TESTNET
-const UniswapV2RouterAddress = "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3";
-const UniswapV2FactoryAddress = "0xb7926c0430afb07aa7defde6da862ae0bde767bc";
+const tokensToMint = toBN(1000000000);
+const reserveTokens = toBN(1000000);
+const OWNER = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 module.exports = async (deployer) => {
   const contractsRegistry = await ContractsRegistry.at((await Proxy.deployed()).address);
+  const uniswapMock = await deployer.deploy(UniswapV2RouterMock);
+
+  const USD = await ERC20Mock.at(await contractsRegistry.getUSDContract());
+  const DEXE = await ERC20Mock.at(await contractsRegistry.getDEXEContract());
+  const WBNB = await deployer.deploy(ERC20Mock, "WBNB", "WBNB", 18);
+  const WETH = await deployer.deploy(ERC20Mock, "WETH", "WETH", 18);
+  const CAKE = await deployer.deploy(ERC20Mock, "CAKE", "CAKE", 18);
+  const SAFEMOON = await deployer.deploy(ERC20Mock, "SAFEMOON", "SAFEMOON", 18);
+
+  await DEXE.mint(OWNER, wei(tokensToMint));
+  await USD.mint(OWNER, wei(tokensToMint));
+  await WBNB.mint(OWNER, wei(tokensToMint));
+  await WETH.mint(OWNER, wei(tokensToMint));
+  await CAKE.mint(OWNER, wei(tokensToMint));
+  await SAFEMOON.mint(OWNER, wei(tokensToMint));
+
+  await DEXE.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(DEXE.address, wei(reserveTokens));
+
+  await USD.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(USD.address, wei(reserveTokens.idiv(2)));
+
+  await WBNB.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(WBNB.address, wei(reserveTokens.idiv(3)));
+
+  await WETH.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(WETH.address, wei(reserveTokens.idiv(3)));
+
+  await CAKE.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(CAKE.address, wei(reserveTokens));
+
+  await SAFEMOON.approve(uniswapMock.address, wei(reserveTokens));
+  await uniswapMock.setReserve(SAFEMOON.address, wei(reserveTokens));
+
+  await uniswapMock.enablePair(DEXE.address, USD.address);
+  await uniswapMock.enablePair(DEXE.address, WBNB.address);
+  await uniswapMock.enablePair(DEXE.address, WETH.address);
+  await uniswapMock.enablePair(DEXE.address, CAKE.address);
+  await uniswapMock.enablePair(DEXE.address, SAFEMOON.address);
+
+  await uniswapMock.enablePair(WBNB.address, USD.address);
+  await uniswapMock.enablePair(WBNB.address, WETH.address);
+  await uniswapMock.enablePair(WBNB.address, CAKE.address);
+  await uniswapMock.enablePair(WBNB.address, SAFEMOON.address);
+
+  await uniswapMock.enablePair(WETH.address, USD.address);
+  await uniswapMock.enablePair(WETH.address, CAKE.address);
+  await uniswapMock.enablePair(WETH.address, SAFEMOON.address);
+
+  await uniswapMock.enablePair(CAKE.address, USD.address);
+  await uniswapMock.enablePair(CAKE.address, SAFEMOON.address);
+
+  await uniswapMock.enablePair(SAFEMOON.address, USD.address);
 
   logTransaction(
-    await contractsRegistry.addContract(await contractsRegistry.UNISWAP_V2_ROUTER_NAME(), UniswapV2RouterAddress),
+    await contractsRegistry.addContract(await contractsRegistry.UNISWAP_V2_ROUTER_NAME(), uniswapMock.address),
     "Add UniswapV2Router"
   );
 
   logTransaction(
-    await contractsRegistry.addContract(await contractsRegistry.UNISWAP_V2_FACTORY_NAME(), UniswapV2FactoryAddress),
+    await contractsRegistry.addContract(await contractsRegistry.UNISWAP_V2_FACTORY_NAME(), uniswapMock.address),
     "Add UniswapV2Factory"
+  );
+
+  logContracts(
+    ["DEXE", DEXE.address],
+    ["USD", USD.address],
+    ["WBNB", WBNB.address],
+    ["WETH", WETH.address],
+    ["CAKE", CAKE.address],
+    ["SAFEMOON", SAFEMOON.address]
   );
 };
