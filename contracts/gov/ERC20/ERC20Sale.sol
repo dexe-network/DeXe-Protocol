85e93349f5bca419657e8694094791123c20ef14
dovgopoly <idovgopoly@gmail.com>
2022-12-26 18:53:11 +0200

    added basic logic to erc20sale

--- contracts/gov/ERC20/ERC20Sale.sol
+++ contracts/gov/ERC20/ERC20Sale.sol
@@ -3,6 +3,48 @@ pragma solidity ^0.8.4;
 
 import "../../interfaces/gov/ERC20/IERC20Sale.sol";
 
-contract ERC20Sale is IERC20Sale {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+contract ERC20Sale is IERC20Sale, ERC20Capped, ERC20Pausable {
+    address public govAddress;
+
+    modifier onlyGov() {
+        require(msg.sender == govAddress, "ERC20Sale: not a Gov contract");
+        _;
+    }
+
+    constructor(
+        ConstructorParams memory params
+    ) ERC20(params.name, params.symbol) ERC20Capped(params.cap) {
+        govAddress = params.govAddress;
+    }
+
+    function mint(address account, uint256 amount) public override onlyGov {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external override onlyGov {
+        _burn(account, amount);
+    }
+
+    function pause() public override onlyGov {
+        _pause();
+    }
+
+    function unpause() public override onlyGov {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
+        super._mint(account, amount);
+    }
 }
--- contracts/interfaces/gov/ERC20/IERC20Sale.sol
+++ contracts/interfaces/gov/ERC20/IERC20Sale.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-interface IERC20Sale {}
+interface IERC20Sale {
+    struct ConstructorParams {
+        string name;
+        string symbol;
+        uint256 cap;
+        uint256 mintedTotal;
+        uint256 saleAmount;
+        uint256[] amounts;
+        address[] users;
+        address govAddress;
+        address saleAddress;
+    }
+
+    function mint(address account, uint256 amount) external;
+
+    function burn(address account, uint256 amount) external;
+
+    function pause() external;
+
+    function unpause() external;
+}
