9342409fbda64f283dd7634c9bb7da83fbbf3304
dovgopoly <idovgopoly@gmail.com>
2023-01-11 18:17:22 +0200

    added erc20sale natspec

--- contracts/interfaces/gov/ERC20/IERC20Sale.sol
+++ contracts/interfaces/gov/ERC20/IERC20Sale.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.8.4;
 
 interface IERC20Sale {
+    /// @notice Initial ERC20Sale parameters. This struct is used as an input argument in the contract constructor
+    /// @param name the name of the token
+    /// @param symbol the symbol of the token
+    /// @param users the list of users for which tokens need to be minted
+    /// @param saleAmount the token amount to be minted for sale
+    /// @param cap cap on the token's total supply
+    /// @param mintedTotal the total amount of tokens to be minted while the contract creation
+    /// @param amounts the list of token amounts which should be minted to the respective users
     struct ConstructorParams {
         string name;
         string symbol;
@@ -12,11 +20,19 @@ interface IERC20Sale {
         uint256[] amounts;
     }
 
+    /// @notice This function is used to mint tokens
+    /// @param account the address to which tokens should be minted
+    /// @param the token amount to be minted
     function mint(address account, uint256 amount) external;
 
+    /// @notice This function is used to burn tokens
+    /// @param account the address from which tokens should be burned
+    /// @param the token amount to be burned
     function burn(address account, uint256 amount) external;
 
+    /// @notice This function is used to trigger stopped contract state
     function pause() external;
 
+    /// @notice This function is used to return default contract state
     function unpause() external;
 }
