1248c0d34ab26f17ed1d81e960a681a0da01877f
dovgopoly <idovgopoly@gmail.com>
2023-01-11 21:39:41 +0200

    added govPool natspec

--- contracts/interfaces/gov/IGovPool.sol
+++ contracts/interfaces/gov/IGovPool.sol
@@ -85,6 +85,11 @@ interface IGovPool {
         uint256[] nftsVoted;
     }
 
+    /// @notice The struct that is used in view functions of contract as a return argument
+    /// @param micropool the address of the delegatee
+    /// @param rewardTokens the list of reward tokens addresses
+    /// @param expectedRewards the list of expected rewards
+    /// @param realRewards the list of real rewards (minimum of expected rewards and governance pool token balances)
     struct UserStakeRewardsView {
         address micropool;
         address[] rewardTokens;
@@ -92,16 +97,27 @@ interface IGovPool {
         uint256[] realRewards;
     }
 
+    /// @notice The struct that holds delegator properties (only for internal needs)
+    /// @param latestCumulativeSum delegator's latest cumulative sum
+    /// @param pending rewards delegator's pending rewards
     struct DelegatorInfo {
         uint256 latestCumulativeSum;
         uint256 pendingRewards;
     }
 
+    /// @notice The struct that holds reward token properties (only for internal needs)
+    /// @param delegators matching delegators addresses with their parameters
+    /// @param cumulativeSum global cumulative sum
     struct RewardTokenInfo {
         mapping(address => DelegatorInfo) delegators;
         uint256 cumulativeSum;
     }
 
+    /// @notice The struct that holds micropool properties (only for internal needs)
+    /// @param totalStake the current total sum of all delegators stakes
+    /// @param rewardTokens the list of reward tokens
+    /// @param rewardTokenInfos matching reward tokens to their parameters
+    /// @param latestDelegatorStake matching delegators to their latest stakes
     struct MicropoolInfo {
         uint256 totalStake;
         EnumerableSet.AddressSet rewardTokens;
@@ -109,18 +125,30 @@ interface IGovPool {
         mapping(address => uint256) latestDelegatorStake;
     }
 
+    /// @notice The struct that holds reward properties (only for internal needs)
+    /// @param onchainRewards matching proposal ids to their rewards
+    /// @param offchainRewards matching off-chain token addresses to their rewards
+    /// @param offchainTokens the list of off-chain token addresses
     struct PendingRewards {
         mapping(uint256 => uint256) onchainRewards;
         mapping(address => uint256) offchainRewards;
         EnumerableSet.AddressSet offchainTokens;
     }
 
+    /// @notice The struct that is used in view functions of contract as a return argument
+    /// @param onchainRewards the list of on-chain rewards
+    /// @param offchainRewards the list of off-chain rewards
+    /// @param offchainTokens the list of off-chain token addresses
     struct PendingRewardsView {
         uint256[] onchainRewards;
         uint256[] offchainRewards;
         address[] offchainTokens;
     }
 
+    /// @notice The struct that holds off-chain properties (only for internal needs)
+    /// @param verifier the off-chain verifier address
+    /// @param resultsHash the ipfs results hash
+    /// @param usedHashes matching hashes to their usage state
     struct OffChain {
         address verifier;
         string resultsHash;
@@ -306,11 +334,18 @@ interface IGovPool {
         address delegatee
     ) external view returns (uint256, ShrinkableArray.UintArray memory);
 
+    /// @notice The function to get onchain and offchain rewards
+    /// @param user the address of the user whose rewards are required
+    /// @param proposalIds the list of proposal ids
+    /// @return the list of rewards
     function getPendingRewards(
         address user,
         uint256[] calldata proposalIds
     ) external view returns (PendingRewardsView memory);
 
+    /// @notice The function to get delegator staking rewards from all micropools
+    /// @param delegator the address of the delegator
+    /// @return the list of rewards
     function getDelegatorStakingRewards(
         address delegator
     ) external view returns (UserStakeRewardsView[] memory);
@@ -324,7 +359,7 @@ interface IGovPool {
     /// @return bytes32 hash
     function getOffchainSignHash(string calldata resultsHash) external view returns (bytes32);
 
-    /// @notice The function to get offchain verifier address
+    /// @notice The function to get off-chain verifier address
     /// @return address of verifier
     function getVerifier() external view returns (address);
 }
