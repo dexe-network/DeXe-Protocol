c7c893e50b9b4b0fb6d8299bff7b28bfbe20ed4f
Ruslan Kasheparov <ruslankasheparov@gmail.com>
2023-06-15 17:02:35 +0200

    add check for proposalId (6.49, 6.50, 6.51, 6.52)

--- test/trader/BasicTraderPool.test.js
+++ test/trader/BasicTraderPool.test.js
@@ -693,6 +693,10 @@ describe("BasicTraderPool", () => {
           proposalPool.changeProposalRestrictions(2, [time.plus(1000000), wei("1000"), wei("10")]),
           "TPRP: proposal doesn't exist"
         );
+        await truffleAssert.reverts(
+          proposalPool.changeProposalRestrictions(0, [time.plus(1000000), wei("1000"), wei("10")]),
+          "TPRP: proposal doesn't exist"
+        );
 
         info = (await proposalPool.getProposalInfos(0, 1))[0];
 
@@ -732,6 +736,7 @@ describe("BasicTraderPool", () => {
         );
 
         await truffleAssert.reverts(investProposal(3, wei("100"), SECOND), "TPRP: proposal doesn't exist");
+        await truffleAssert.reverts(investProposal(0, wei("100"), SECOND), "TPRP: proposal doesn't exist");
         await truffleAssert.reverts(investProposal(1, wei("100"), SECOND), "TPRP: proposal is closed");
         await truffleAssert.reverts(investProposal(2, wei("100"), SECOND), "TPRP: proposal is overinvested");
       });
@@ -1052,6 +1057,7 @@ describe("BasicTraderPool", () => {
         await investProposal(1, wei("500"), SECOND);
 
         await truffleAssert.reverts(reinvestProposal(2, wei("250"), OWNER), "TPRP: proposal doesn't exist");
+        await truffleAssert.reverts(reinvestProposal(0, wei("250"), OWNER), "TPRP: proposal doesn't exist");
         await truffleAssert.reverts(reinvestProposal(1, wei("1000"), SECOND), "TPRP: divesting more than balance");
       });
 
@@ -1280,6 +1286,11 @@ describe("BasicTraderPool", () => {
 
         await truffleAssert.reverts(
           exchangeFromExactProposal(2, tokens.WETH.address, wei("250")),
+          "TPRP: proposal doesn't exist"
+        );
+
+        await truffleAssert.reverts(
+          exchangeFromExactProposal(0, tokens.WETH.address, wei("250")),
           "TPRP: proposal doesn't exist"
         );
 
--- test/trader/InvestTraderPool.test.js
+++ test/trader/InvestTraderPool.test.js
@@ -410,6 +410,7 @@ describe("InvestTraderPool", () => {
         await createProposal(wei("500"), [time.plus(1000), wei("500")]);
 
         await truffleAssert.reverts(investProposal(3, wei("100"), SECOND), "TPIP: proposal doesn't exist");
+        await truffleAssert.reverts(investProposal(0, wei("100"), SECOND), "TPIP: proposal doesn't exist");
         await truffleAssert.reverts(investProposal(1, wei("100"), SECOND), "TPIP: proposal is closed");
         await truffleAssert.reverts(investProposal(2, wei("100"), SECOND), "TPIP: proposal is overinvested");
       });
@@ -497,6 +498,10 @@ describe("InvestTraderPool", () => {
 
         await truffleAssert.reverts(
           proposalPool.changeProposalRestrictions(2, [time.plus(1000000), wei("1000")]),
+          "TPIP: proposal doesn't exist"
+        );
+        await truffleAssert.reverts(
+          proposalPool.changeProposalRestrictions(0, [time.plus(1000000), wei("1000")]),
           "TPIP: proposal doesn't exist"
         );
 
@@ -690,6 +695,7 @@ describe("InvestTraderPool", () => {
 
       it("should check withdrawal in proposal", async () => {
         await truffleAssert.reverts(withdrawProposal(2, wei("900")), "TPIP: proposal doesn't exist");
+        await truffleAssert.reverts(withdrawProposal(0, wei("900")), "TPIP: proposal doesn't exist");
       });
     });
 
@@ -714,6 +720,11 @@ describe("InvestTraderPool", () => {
       it("should check supply in proposal", async () => {
         await truffleAssert.reverts(
           supplyProposal(2, [wei("50"), wei("50")], [tokens.WETH.address, tokens.MANA.address]),
+          "TPIP: proposal doesn't exist"
+        );
+
+        await truffleAssert.reverts(
+          supplyProposal(0, [wei("50"), wei("50")], [tokens.WETH.address, tokens.MANA.address]),
           "TPIP: proposal doesn't exist"
         );
 
@@ -904,10 +915,12 @@ describe("InvestTraderPool", () => {
 
         await truffleAssert.reverts(reinvestProposal(1, SECOND), "TPIP: nothing to divest");
         await truffleAssert.reverts(reinvestProposal(2, SECOND), "TPIP: proposal doesn't exist");
+        await truffleAssert.reverts(reinvestProposal(0, SECOND), "TPIP: proposal doesn't exist");
       });
 
       it("should check reinvest in proposal", async () => {
         await truffleAssert.reverts(convertToDividends(2), "TPIP: proposal doesn't exist");
+        await truffleAssert.reverts(convertToDividends(9), "TPIP: proposal doesn't exist");
         await truffleAssert.reverts(reinvestProposal(1, OWNER), "TPIP: nothing to divest");
       });
 
