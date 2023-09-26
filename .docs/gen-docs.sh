8008e304d5168e925abab0524d5984ff9789fc2a
dovgopoly <idovgopoly@gmail.com>
2023-09-26 19:12:22 +0300

    added docs script

--- .docs/gen-docs.sh
+++ .docs/gen-docs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+rm -rf generated-markups
+
 npm run generate-docs
 
 echo "markups have been generated. Moving..."
 
 mkdir -p docs
-rsync -av --delete ./generated-markups/contracts/interfaces/ ./docs
+rsync -av --delete generated-markups/contracts/interfaces/ docs
+
+rm -r generated-markups
