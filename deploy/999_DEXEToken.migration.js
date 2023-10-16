2b7a28524cece54702f4c3b0cf3d9148f6427a71
dovgopoly <idovgopoly@gmail.com>
2023-10-16 21:25:42 +0300

    added log

--- deploy/999_DEXEToken.migration.js
+++ deploy/999_DEXEToken.migration.js
@@ -5,5 +5,7 @@ const config = require("./config/config.json");
 module.exports = async (deployer, logger) => {
   const dexeToken = await deployer.deploy(DEXE, "DEXE", "DEXE");
 
+  logger.logContracts(["DEXEToken", dexeToken.address]);
+
   logger.logTransaction(await dexeToken.addOwners(config.dexeTokenOwners), "Adding DEXE token owners");
 };
