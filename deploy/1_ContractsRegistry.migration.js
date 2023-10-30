0d5e3e5708da72b743db4a9d603e6f1705c37954
Artem Chystiakov <artem.ch31@gmail.com>
2023-10-30 15:50:51 +0200

    fixed migrations

--- deploy/1_ContractsRegistry.migration.js
+++ deploy/1_ContractsRegistry.migration.js
@@ -3,7 +3,7 @@ const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 
 module.exports = async (deployer) => {
   const contractsRegistry = await deployer.deploy(ContractsRegistry);
-  await deployer.deploy(ERC1967Proxy, [contractsRegistry.address, "0x"], { misc: "proxy" });
+  await deployer.deploy(ERC1967Proxy, [contractsRegistry.address, "0x"], { name: "proxy" });
 
   await (await deployer.deployed(ContractsRegistry, "proxy")).__OwnableContractsRegistry_init();
 };
