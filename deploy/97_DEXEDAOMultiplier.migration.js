53127ea5d39485571af7f70d250bb20419167c94
Dmitry Redkin <tanatonaut@gmail.com>
2023-12-04 18:10:29 +0200

    added empty transactions

--- deploy/97_DEXEDAOMultiplier.migration.js
+++ deploy/97_DEXEDAOMultiplier.migration.js
@@ -4,6 +4,10 @@ const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 const DexeMultiplier = artifacts.require("DexeERC721Multiplier");
 
 module.exports = async (deployer) => {
+  // Empty transactions to make nonces be fit with the BSC mainnet deploy
+  await deployer.sendNative("0x0000000000000000000000000000000000000000", 0);
+  await deployer.sendNative("0x0000000000000000000000000000000000000000", 1);
+
   let dexeMultiplier = await deployer.deploy(DexeMultiplier);
   await deployer.deploy(ERC1967Proxy, [dexeMultiplier.address, "0x"], { name: "multiplierProxy" });
 
