54f6eb8db99562a52508f24d5ce206f1848b4aa7
Dmitry Redkin <tanatonaut@gmail.com>
2024-03-08 10:34:00 +0200

    removed commented libs

--- contracts/libs/gov/token-sale-proposal/TokenSaleProposalModify.sol
+++ contracts/libs/gov/token-sale-proposal/TokenSaleProposalModify.sol
@@ -1,31 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
-
-// import "@solarity/solidity-lib/libs/utils/TypeCaster.sol";
-// import "@solarity/solidity-lib/libs/utils/DecimalsConverter.sol";
 
 import "../../../interfaces/gov/proposals/ITokenSaleProposal.sol";
 
-// import "../../../interfaces/gov/IGovPool.sol";
-// import "../../../interfaces/gov/user-keeper/IGovUserKeeper.sol";
-
-// import "../../../core/CoreProperties.sol";
-// import "../../../gov/proposals/TokenSaleProposal.sol";
-
-// import "../../../libs/math/MathHelper.sol";
-// import "../../../libs/utils/TypeHelper.sol";
-
 library TokenSaleProposalModify {
-    // using MathHelper for uint256;
-    // using DecimalsConverter for *;
-    // using TypeCaster for *;
-    // using TypeHelper for *;
-    // using SafeERC20 for IERC20;
-    // using EnumerableSet for *;
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
     function changeParticipationDetails(
